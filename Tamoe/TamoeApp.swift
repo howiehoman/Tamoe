bc7e16cd00761f18d41e717a05ac2f12923fbf2d
Howie Homan <howiehoman08@gmail.com>
2026-07-03 15:30:24 +0700

    Initial Commit

---
+++ Tamoe/TamoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamoeApp.swift
+//  Tamoe
+//
+//  Created by Howie Homan on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TamoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
