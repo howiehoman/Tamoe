551b1e75ff27fa7216d08d2eeec5ae13964cdf98
Hansel Meinhard <hansel.meinhard04@gmail.com>
2026-07-14 16:19:46 +0700

    init: Data Models dan yang berhubungan

--- Tamoe/Core/Domain/TamoeError.swift
+++ Tamoe/Core/Domain/TamoeError.swift
@@ -1,1 +1,16 @@
+import Foundation
 
+enum TamoeError: Error, Equatable, Sendable {
+    case planNotFound
+    case eventNotFound(UUID)
+    case categoryNotFound(UUID)
+    case guestNotFound(UUID)
+    case duplicateCategoryName
+    case categoryInUse(categoryID: UUID)
+    case categoryEventInUse(categoryID: UUID, eventID: UUID)
+    case capacityBelowAllocatedQuota(eventID: UUID)
+    case eventInUse(eventID: UUID, hasCategoryAllocations: Bool, hasGuestAssignments: Bool)
+    case assignmentNotFound(guestID: UUID, eventID: UUID)
+    case missingCategory(UUID)
+    case categoryUnavailableForEvent(categoryID: UUID, eventID: UUID)
+}
--- Tamoe/Data/Models/GuestNote.swift
+++ Tamoe/Data/Models/GuestNote.swift
@@ -1,1 +1,19 @@
+import Foundation
+import SwiftData
 
+@Model
+final class GuestNote {
+    @Attribute(.unique) var id: UUID
+    var body: String
+    var sortIndex: Int
+    
+    @Relationship(inverse: \Guest.notes)
+    var guest: Guest?
+    
+    init(id: UUID = UUID(), body: String, sortIndex: Int, guest: Guest) {
+        self.id = id
+        self.body = body.trimmingCharacters(in: .whitespaces)
+        self.sortIndex = sortIndex
+        self.guest = guest
+    }
+}
